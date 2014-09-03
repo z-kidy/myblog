e3303cb480638f650a405c2e4543b107fd4b5f02
z-kidy <zjindiss@gmail.com>
2014-09-03 21:10:05 +0800

    4degree

--- blog.py
+++ blog.py
@@ -1,22 +1,28 @@
 import os
 import re
 from string import letters
-
+import random
 import webapp2
 import jinja2
 import time
 from google.appengine.ext import db
 import hashlib
+import hmac
+
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+SECRET = "zkidy" 
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+
 def hash_str(s):
-    return hashlib.md5(s).hexdigest()
+    return hmac.new(SECRET,s).hexdigest()
 
 def make_secure_val(s):
     return "%s|%s" % (s,hash_str(s))
@@ -26,12 +32,50 @@ def check_secure_val(h):
     if h == make_secure_val(val) :
         return val
 
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
 class BaseHandler(webapp2.RequestHandler):
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        return render_str(template, **params)
+
     def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
+        self.write(self.render_str(template, **kw))
 
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+        
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+    
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+        #return check_secure_val(cookie_val) and cookie_val 
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
 
 class FirstPage(BaseHandler):
     def get(self):
@@ -75,54 +119,136 @@ def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
 class Signup(BaseHandler):
-
     def get(self):
         self.render("signup-form.html")
 
     def post(self):
         have_error = False
-        username = self.request.get('username')
-        password = self.request.get('password')
-        verify = self.request.get('verify')
-        email = self.request.get('email')
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
 
-        params = dict(username = username,
-                      email = email)
+        params = dict(username = self.username,
+                      email = self.email)
 
-        if not valid_username(username):
+        if not valid_username(self.username):
             params['error_username'] = "That's not a valid username."
             have_error = True
 
-        if not valid_password(password):
+        if not valid_password(self.password):
             params['error_password'] = "That wasn't a valid password."
             have_error = True
-        elif password != verify:
+        elif self.password != self.verify:
             params['error_verify'] = "Your passwords didn't match."
             have_error = True
 
-        if not valid_email(email):
+        if not valid_email(self.email):
             params['error_email'] = "That's not a valid email."
             have_error = True
 
         if have_error:
             self.render('signup-form.html', **params)
         else:
-            self.redirect('/unit2/welcome?username=' + username)
+            self.done()
+    def done(self, *a, **kw):
+        raise NotImplementedError
+
+class Unit2Signup(Signup):
+    def done(self):
+        self.redirect('/unit2/welcome?username=' + self.username )
+
+class Register(Signup):
+    def done(self):
+        #make sure the user doesn't already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('signup-form.html', error_username = msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/blog')
+
+class Login(BaseHandler):
+    def get(self):
+        self.render("login-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        u = User.login(username, password)
+        if u:
+            self.login(u)
+            self.redirect('/blog')
+        else:
+            msg = 'Invalid login'
+            self.render('login-form.html', error = msg)
+
+class Logout(BaseHandler):
+    def get(self):
+        self.logout()
+        self.redirect('/blog')
 
 class Welcome(BaseHandler):
     def get(self):
-        username = self.request.get('username')
+        username = self.request.cookies.get('user_id')
+        username = username.split('|')[0]
+
         if valid_username(username):
             self.render('welcome.html', username = username)
         else:
             self.redirect('/unit2/signup')
+
+class Unit3Welcome(BaseHandler):
+    def get(self):
+        if self.user:
+            self.render('welcome.html', username = self.user.name)
+        else:
+            self.redirect('/signup')
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email    = db.StringProperty()
+    
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid)
+    
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u 
 
 class Art(db.Model):
     title   = db.StringProperty(required = True)
     art     = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
-class MainPage(BaseHandler):
+class Article(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+class Unit3art(BaseHandler):
     def render_front(self,title='',art='',error=''):
         arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
         self.render("art.html",title=title,art = art ,error = error,arts = arts)
@@ -142,13 +268,7 @@ class MainPage(BaseHandler):
         else:
             error = "We need both title and art "
             self.render_front("art.html",error = error,title= title,art = art)
-
-class Article(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-    
+  
 class Blog(BaseHandler):
     def get(self):
         articles = db.GqlQuery("SELECT * FROM Article ORDER BY created DESC")
@@ -177,7 +297,7 @@ class Newpost(BaseHandler):
             error = "We need both subject and article "
             self.render_front("newpost.html",error = error,subject= subject,content = content)
 
-class Newarticle(BaseHandler):
+class PostPage(BaseHandler):
     def get(self,post_id):
         p = Article.get_by_id( int(post_id) )
 
@@ -192,10 +312,17 @@ class Newarticle(BaseHandler):
 
 app = webapp2.WSGIApplication([ ('/',FirstPage),
                                 ('/unit2/rot13', Rot13),
-                                ('/unit2/signup', Signup),
+                                ('/unit2/Unit2Signup', Unit2Signup),
                                 ('/unit2/welcome', Welcome),
-                                ('/unit3/art',MainPage),
+                                ('/unit3/art',Unit3art),
                                 ('/blog',Blog),
-                                ('/blog/(\d+)',Newarticle),
-                                ('/blog/newpost',Newpost)],
+                                ('/blog/(\d+)',PostPage),
+                                ('/blog/newpost',Newpost),
+                                ('/signup',Register),
+                                ('/login',Login),
+                                ('/logout',Logout),
+                                ('/unit3/welcome', Unit3Welcome),
+                                ('/login',Login)
+                                ],
+
                               debug=True)
